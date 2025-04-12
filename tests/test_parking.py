7b8ff4b31a55a3f532c2b58b0544963feba4d3c2
arlauskaitekristina <107872404+arlauskaitekristina@users.noreply.github.com>
2025-04-12 21:00:45 +0300

    Update test_parking.py

--- tests/test_parking.py
+++ tests/test_parking.py
@@ -4,7 +4,7 @@ import factory
 import pytest
 from sqlalchemy import func, select
 
-from ..app.models import Client, ClientParking, Parking
+from app.models import Client, ClientParking, Parking
 
 from .factories import ClientFactory, ParkingFactory
 
